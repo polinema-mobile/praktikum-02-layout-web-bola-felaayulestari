a1fa0e4e0edafaab8d37d32f507dec016d0ca15d
felaayulestari <felaayulestari4940@gmail.com>
2021-02-16 17:31:46 +0700

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,28 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Contoh Stack Widget"),
+          title: Text("Contoh ListView Widget"),
         ),
-        body: Stack(
+        body: ListView(
           children: <Widget>[
             Container(
               color: Colors.green,
-              alignment: Alignment.bottomCenter,
-              child: Text("Satu",
+              alignment: Alignment.topLeft,
+              child: Text("ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                   style: TextStyle(fontSize: 30, color: Colors.white)),
             ),
             Container(
               color: Colors.red,
-              alignment: Alignment.bottomCenter,
-              child: Text("Dua",
+              alignment: Alignment.topLeft,
+              child: Text("ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                   style: TextStyle(fontSize: 30, color: Colors.white)),
               height: 400.0,
               width: 300.0,
             ),
             Container(
               color: Colors.deepPurple,
-              alignment: Alignment.bottomCenter,
-              child: Text("Tiga",
+              alignment: Alignment.topLeft,
+              child: Text("ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                   style: TextStyle(fontSize: 30, color: Colors.white)),
               height: 200.0,
               width: 200.0,
