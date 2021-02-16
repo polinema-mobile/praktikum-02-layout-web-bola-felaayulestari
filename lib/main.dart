83e67362fe1c27149e213de2d8f3e92ef13d3c58
felaayulestari <felaayulestari4940@gmail.com>
2021-02-17 00:05:34 +0700

    Image Widget Success

--- lib/main.dart
+++ lib/main.dart
@@ -6,62 +6,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Contoh GridView Widget"),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: <Widget>[
-            Container(
-              color: Colors.green,
-              alignment: Alignment.center,
-              child: Text("1",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-            ),
-            Container(
-              color: Colors.red,
-              alignment: Alignment.center,
-              child: Text("2",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 400.0,
-              width: 300.0,
-            ),
-            Container(
-              color: Colors.deepPurple,
-              alignment: Alignment.center,
-              child: Text("3",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.blueGrey,
-              alignment: Alignment.center,
-              child: Text("4",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.cyan[800],
-              alignment: Alignment.center,
-              child: Text("5",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-            Container(
-              color: Colors.yellow[800],
-              alignment: Alignment.center,
-              child: Text("6",
-                  style: TextStyle(fontSize: 30, color: Colors.white)),
-              height: 200.0,
-              width: 200.0,
-            ),
-          ],
-        ),
+      home: Image(
+        image: NetworkImage('https://pixnio.com/free-images/2021/02/15/2021-02-15-10-38-28-1200x1800.jpg'),
       ),
     );
   }
