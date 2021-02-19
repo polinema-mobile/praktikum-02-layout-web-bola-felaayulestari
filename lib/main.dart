1db5a4f374d8ab8d3fa977d10e3164f09970576d
felaayulestari <felaayulestari4940@gmail.com>
2021-02-19 09:34:49 +0700

    praktikum dua

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,47 @@ class MyApp extends StatelessWidget {
                 ]),
               ],
             ),
+            SizedBox(
+              width: 2
+            ),
+            Row(
+              children: [
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      color: Colors.purpleAccent,
+                      alignment: Alignment.bottomLeft,
+                      child: Text("Transfer",
+                        style: TextStyle(
+                          fontSize: 15, color: Colors.black
+                        )
+                      ),
+                      width: 200.0,
+                    ),
+                    Container(
+                      color: Colors.white,
+                      alignment: Alignment.bottomCenter,
+                      child: Text("Costa Mendekat Ke Palmeiras",
+                          style: TextStyle(
+                          fontSize: 30, color: Colors.black
+                          ),
+                        ),
+                      width: 150.0,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: NetworkImage('https://d10dnch8g6iuzs.cloudfront.net/picture/11720210213072120788'),
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                      width: 150.0,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            
           ],
         ),
       ),
