8d3f4ad2f12f665f5e62405a85c7b81b9fc591f3
felaayulestari <felaayulestari4940@gmail.com>
2021-02-20 13:49:08 +0700

    berita utama

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,36 @@ class MyApp extends StatelessWidget {
                     ],
                   ),
                 ),
+                SizedBox(),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple[300])
+                      ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network('https://www.spurs-web.com/static/uploads/2019/07/skysports-diego-costa-atletico-madrid_4644146.jpg'),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text(
+                            "Costa Mendekat Ke Palmeiras",
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                            ),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.fromLTRB(8,12,336,12),
+                        color: Colors.purple[300],
+                        child: Text(
+                          "Transfer",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
